d43c174689198ed7397b76c17f407622a48b6b93
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-09 16:09:19 +0000

    Ex10 Solved

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -7,7 +7,19 @@ namespace Triangle
         static void Main(string[] args)
         {
             int x = int.Parse(args[0]);
-            char a = char.Parse(args[2]);
+            char a = char.Parse(args[1]);
+            for (int i = 0; i <= x; i++)
+            {
+                for (int j = 1; j <= i; j++)
+
+                {
+                    Console.Write(a);
+                }
+                Console.WriteLine();
+            }
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(a);
         }
     }
 }
