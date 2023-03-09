2da47a96db41981fecd23228b966e7bdd7bbf172
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-09 13:46:30 +0000

    Max and Min Values

---
+++ Specials/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Maximo Int = {int.MaxValue}");
+            Console.WriteLine($"Minimo Int = {int.MinValue}");
+            Console.WriteLine($"Maximo Double = {double.MaxValue}");
+            Console.WriteLine($"Minimo Double = {double.MinValue}");
+            Console.WriteLine($"Maximo Sbyte = {sbyte.MaxValue}");
+            Console.WriteLine($"Minimo Sbyte = {sbyte.MinValue}");
+            Console.WriteLine($"Maximo Long = {long.MaxValue}");
+            Console.WriteLine($"Minimo Long = {long.MinValue}");
+            Console.WriteLine($"Maximo Float = {float.MaxValue}");
+            Console.WriteLine($"Minimo Float = {float.MinValue}");
+
+        }
+    }
+}
