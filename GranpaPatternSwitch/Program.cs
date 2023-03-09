2947cec9990340f1002c306299b3a781bbfa5ccc
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-09 15:20:31 +0000

    Switch sintax used correctly

---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Place your question? ");
+            string opcao = Console.ReadLine();
+
+
+            //string opcao;
+            string message = opcao switch
+            {
+
+                "How are you" => "I'm fine, thank you",
+                "What's your name?" => "Gran'pa",
+                "What's your mission?" => "Destroy mankind!",
+                null => "You got me, I'm not THAT smart!",
+                _ => "Not possible"
+            };
+
+            Console.WriteLine(message);
+        }
+    }
+}
