094aecf4c90e680883ffd6aac282745548be1267
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-09 15:54:16 +0000

    int and char converted

---
+++ Triangle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(args[0]);
+            char a = char.Parse(args[2]);
+        }
+    }
+}
