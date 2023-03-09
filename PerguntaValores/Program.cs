148eed52fc7f4b33c0c4e8d26ae893e4780ae4e0
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-09 13:36:26 +0000

    All stuff add .

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedir e converter valores
+            Console.WriteLine("Inserir Altura");
+            double Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inserir Raio");
+            double Raio = double.Parse(Console.ReadLine());
+
+            //Calcular Valores
+            double Volume = Math.PI * Raio * Raio * Altura;
+            double Superficie = 2 * Math.PI * Raio * (Raio + Altura);
+
+            //Imprimir valores
+            Console.WriteLine($"Volume é = {Volume:f2}");
+            Console.WriteLine($"Area de superficie é = {Superficie:f2}");
+        }
+    }
+}
