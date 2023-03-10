9037049c9aac1f1ef77ea8ad9ada4a67c6e2f316
Butchsss <rodrigomarques23work@gmail.com>
2023-03-10 14:12:08 +0000

    Cross Draw corrected + comments

--- CrossSquare/Program.cs
+++ CrossSquare/Program.cs
@@ -6,20 +6,21 @@ namespace CrossSquare
     {
         static void Main(string[] args)
         {
-            
-            int userInput = Convert.ToInt32(args[0]);
 
-            for(int i = 0; i < userInput; i++)
+            int userInput = Convert.ToInt32(args[0]);//Convert Input to Int
+
+            for (int i = 0; i < userInput; i++)
             {
-                for(int j = 0; j < userInput; j++)
-                    if(i  == 0 || i == userInput - 1 || j == 0 ||
-                        j == userInput           - 1)
+                for (int j = 0; j < userInput; j++)
+                    if (i == 0 || i == userInput - 1 || j == 0 ||
+                        j == userInput - 1)
                     {
                         Console.Write("#");
                     }
                     else
                     {
-                        if(userInput < 7) Console.Write(" ");
+                        if (userInput < 7) Console.Write("#");// if input<7 only 
+                        //draw #
                         else
                             Console.Write(j - i == 0 || j + i == userInput - 1 ?
                                 "X" : " ");
