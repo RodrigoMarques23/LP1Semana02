038c07b4577a7f72b5c9664ea7c8233d3d81aa67
Butchsss <rodrigomarques23work@gmail.com>
2023-03-10 14:06:02 +0000

    Ex11, border draw, X draw

---
+++ CrossSquare/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CrossSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int userInput = Convert.ToInt32(args[0]);
+
+            for(int i = 0; i < userInput; i++)
+            {
+                for(int j = 0; j < userInput; j++)
+                    if(i  == 0 || i == userInput - 1 || j == 0 ||
+                        j == userInput           - 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        if(userInput < 7) Console.Write(" ");
+                        else
+                            Console.Write(j - i == 0 || j + i == userInput - 1 ?
+                                "X" : " ");
+                    }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
